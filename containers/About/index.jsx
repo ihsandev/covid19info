d4ef02d352275b74d5aacbbd1f9b24063dc25b96
Ihsan <ihksansanhas@gmail.com>
2020-04-03 08:00:45 +0700

    update about

--- containers/About/index.jsx
+++ containers/About/index.jsx
@@ -20,6 +20,7 @@ const AboutPage = () => (
         <div>
             <p>Aplikasi Web ini menyajikan informasi seputar perkembangan Jumlah 
                 COVID 19 / Virus Corona berupa yang terkonfirmasi, sembuh, dan meninggal
+                dan juga berita terkait Virus Corona yang diambil dari <b onClick={() => window.open('https://www.detik.com/tag/news/virus-corona/')}>detik.com</b>
             </p>
         </div>
         <div style={{marginTop: 20}}>
@@ -32,6 +33,10 @@ const AboutPage = () => (
         <div style={{marginTop: 20}}>
             <div>Source API :</div>
             <Url onClick={() => window.open('https://github.com/mathdroid/covid-19-api')}>https://github.com/mathdroid/covid-19-api</Url>
+        </div>
+        <div style={{marginTop: 20}}>
+            <div>Referensi News / Berita :</div>
+            <Url onClick={() => window.open('https://www.detik.com/tag/news/virus-corona/')}>https://www.detik.com/tag/news/virus-corona/</Url>
         </div>
     </Layout>
 )
