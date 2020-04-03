b7859621e0eecf1a460a27f3430453e30a70c9f1
Ihsan <ihksansanhas@gmail.com>
2020-04-03 11:27:38 +0700

    update view

--- containers/Home/partials/timeline.jsx
+++ containers/Home/partials/timeline.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
+import Link from 'next/link'
 import NewsList from '../../../components/NewsList'
 
 const WrapTimeLine = styled.div`
    display: grid;
-   grid-template-columns: 1.2fr 1fr;
+   grid-template-columns: 1fr 1.3fr;
    grid-gap: 1rem;
    min-height: 400px;
    overflow: hidden;
@@ -32,11 +33,47 @@ const WrapTimeLine = styled.div`
    }
 `
 
+const TimeLineTitle = styled.div`
+  display: flex;
+  align-items: baseline;
+  justify-content: space-between;
+  > span {
+    a {
+      color: #232323;
+      font-size: 0.9em;
+      text-decoration: underline;
+      cursor: pointer;
+      &:hover {
+        color: #bb3535;
+      }
+    }
+  }
+`
+
 const TimeLine = ({ newsData }) => {
     return (
-        <div>
-            <h1 style={{marginBottom: 20}}>Covid News</h1>
+        <div style={{marginBottom: 30}}>
+          <TimeLineTitle>
+            <h1 style={{marginBottom: 20}}>Covid Timeline</h1>
+            <span>
+              <Link href="/covid-news">
+                <a>
+                  More Covid News
+                </a>
+              </Link>
+            </span>
+          </TimeLineTitle>
             <WrapTimeLine>
+                <div>
+                    <iframe  
+                        width="100%"
+                        height="100%" 
+                        src="https://www.youtube.com/embed/1APwq1df6Mw" 
+                        frameborder="0" 
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                        allowfullscreen 
+                    />
+                </div>
                 <div className="list-news-timeline">
                     {
                     newsData && newsData.map((item, i) => {
@@ -52,16 +89,6 @@ const TimeLine = ({ newsData }) => {
                         )
                     })
                     }
-                </div>
-                <div>
-                    <iframe  
-                        width="100%"
-                        height="100%" 
-                        src="https://www.youtube.com/embed/1APwq1df6Mw" 
-                        frameborder="0" 
-                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
-                        allowfullscreen 
-                    />
                 </div>
             </WrapTimeLine>
         </div>
